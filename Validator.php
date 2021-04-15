b450929772351423db93be81e7fa9bc7849adced
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-04-15 17:41:29 -0600

    Prepare for converting to ExtensionRegistry

--- Validator.php
+++ Validator.php
@@ -7,32 +7,38 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 
+// remove?
 if ( defined( 'ParamProcessor_VERSION' ) ) {
 	// Do not initialize more than once.
 	return 1;
 }
 
+// extension.json callback key and callback function elsewhere
 define( 'Validator_VERSION', '2.2.4' );
 define( 'ParamProcessor_VERSION', Validator_VERSION ); // @deprecated since 1.0
 
 // Internationalization
+// extension.json MessagesDirs key
 $GLOBALS['wgMessagesDirs']['Validator'] = __DIR__ . '/i18n';
 
 
 $GLOBALS['wgExtensionFunctions'][] = function () {
+	// extension.json requires, mediawiki key:
 	if ( version_compare( $GLOBALS['wgVersion'], '1.23c', '<' ) ) {
 		die( '<b>Error:</b> This version of Validator requires MediaWiki 1.23 or above.' );
 	}
 
+	// extension.json load_composer_autoloader key:
 	if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 		include_once( __DIR__ . '/vendor/autoload.php' );
 	}
 
+	// extension.json callback key and callback function elsewhere? Or ExtensionFunctions key?
 	if ( !class_exists( ParamProcessor\Processor::class ) ) {
 		throw new Exception( 'Validator depends on the ParamProcessor library.' );
 	}
 
-	// Display extension information
+	// extension.json relevant keys:
 	$GLOBALS['wgExtensionCredits']['other'][] = [
 		'path' => __FILE__,
 		'name' => 'Validator',
@@ -56,6 +62,7 @@ $GLOBALS['wgExtensionFunctions'][] = function () {
 	 *
 	 * @return boolean
 	 */
+	// extension.json hooks key and move to function elsewhere:
 	$GLOBALS['wgHooks']['UnitTestsList'][]	= function( array &$files ) {
 		// @codeCoverageIgnoreStart
 		$directoryIterator = new RecursiveDirectoryIterator( __DIR__ . '/tests/phpunit/' );
@@ -73,6 +80,7 @@ $GLOBALS['wgExtensionFunctions'][] = function () {
 		// @codeCoverageIgnoreEnd
 	};
 
+	// extension.json callback key and callback function elsewhere
 	$GLOBALS['wgDataValues']['mediawikititle'] = ParamProcessor\MediaWikiTitleValue::class;
 
 	$GLOBALS['wgParamDefinitions']['title'] = [
